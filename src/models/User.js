4b908545bda626bfdfeccb8e3822d2f7a268c58a
vhanok70 <vhanok70@gmail.com>
2026-01-16 15:17:13 +0530

    Create User.js

---
+++ src/models/User.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  mobile: { type: String, unique: true, index: true },
+  name: { type: String },
+  email: { type: String },
+  kycStatus: {
+    type: String,
+    enum: ["PENDING", "VERIFIED", "REJECTED"],
+    default: "PENDING"
+  },
+  walletBalance: { type: Number, default: 0 },
+  isActive: { type: Boolean, default: true }
+}, { timestamps: true });
+
+module.exports = mongoose.model("User", UserSchema);
