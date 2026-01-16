afe080d3e1e5378bba72c5388b6419eb90f98adf
vhanok70 <vhanok70@gmail.com>
2026-01-16 16:13:27 +0530

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,16 +1,21 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-  mobile: { type: String, unique: true, index: true },
-  name: { type: String },
-  email: { type: String },
-  kycStatus: {
-    type: String,
-    enum: ["PENDING", "VERIFIED", "REJECTED"],
-    default: "PENDING"
+const UserSchema = new mongoose.Schema(
+  {
+    mobile: { type: String, unique: true, index: true, required: true },
+    name: String,
+    email: String,
+
+    kycStatus: {
+      type: String,
+      enum: ["PENDING", "VERIFIED", "REJECTED"],
+      default: "PENDING"
+    },
+
+    walletBalance: { type: Number, default: 0 },
+    isActive: { type: Boolean, default: true }
   },
-  walletBalance: { type: Number, default: 0 },
-  isActive: { type: Boolean, default: true }
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", UserSchema);
