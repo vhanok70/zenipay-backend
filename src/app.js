6f39578164bee1e3836c60d238ea713671402070
vhanok70 <vhanok70@gmail.com>
2026-01-16 16:03:03 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ app.use(express.json());
 
 // Routes
 app.use("/auth", require("./routes/auth"));
+app.use("/transactions", require("./routes/transactions"));
 
 // Health check
 app.get("/", (req, res) => {
